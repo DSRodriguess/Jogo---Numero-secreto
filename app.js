7a61ed21e75a92ffb897392aee6a2b5570817dcd
David da Silva Rodrigues <rodrigues.david@graduao.uerj.br>
2024-08-19 15:11:00 -0300

    atualização com o numero secreto seja aleatorio entre 1 e 30

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-alert('Boas vindas ao jogo do numero secreto!');
-let numeroSecreto = 29;
+alert('Boas vindas ao jogo do número secreto!');
+
+let numeroSecreto = Math.floor(Math.random() * 30) + 1;
+
 console.log(numeroSecreto);
-let chute 
+let chute;
+let tentativas = 1;
 
-while(chute != numeroSecreto){
-    chute = prompt('Escolha um numero entre 0 e 30');
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 30');
 
-    if(chute == numeroSecreto){
-        alert(`Parabéns, você descobriu o numero secreto! ${numeroSecreto}`);
-    }
-    else{
-        if(chute > numeroSecreto){
-        alert(`O numero secreto é maior que ${chute}`);
-        } else{
-            alert(`O numero secreto é menor que ${chute}`);
+    if (chute == numeroSecreto) {
+        alert(`Parabéns, você descobriu o número secreto! ${numeroSecreto} com ${tentativas} tentativas.`);
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
         }
+        tentativas++;
     }
 }
-
